b6d3b8a8a37b9c68f35e0de67833f4b8e3812594
Openplugins <206189568@etlgr.com>
2017-02-08 22:16:06 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1672,7 +1672,7 @@ tdcli.sendMessage(chat_id,msg.id_,0,'Show Edit Was Enable..!',1,'md')
 end
 			---------- shit --------
 elseif text == 'editcap' then
-tdcli.editMessageCaption(chat_id, reply_id, nil, 'This is a caption')
+tdcli.editMessageCaption(msg.chat_id_,msg.reply_to_message_id, nil, 'This is a caption')
 end
 elseif text:lower() == 'lock links' and mod(data) then
 if db:get('links:Lock:'..chat_id) == 'Lock' then
